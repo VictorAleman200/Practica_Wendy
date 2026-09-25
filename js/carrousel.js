8326aed42e5ec617d581b9cbfc12e80fc78e3ab3
VictorAleman200 <victor.alemanhidalgo@gmail.com>
2026-09-25 01:10:13 +0000

    Co-authored-by: CarolinaAlvarez1225 <CarolinaAlvarez1225@users.noreply.github.com>

--- js/carrousel.js
+++ js/carrousel.js
@@ -1,36 +1,45 @@
-document.addEventListener("DOMContentLoaded", function(){
- 
-const track = document.querySelector(".track");
-const slides = document.querySelectorAll(".slide");
-const prev = document.querySelector (".prev");
-const next = document.querySelector(".next");
+    document.addEventListener("DOMContentLoaded", function () {
+    const carousels = document.querySelectorAll(".carrousel");
 
-let index = 0; 
-const slideWidth = 100;
- 
-function autoSlide(){
-    const move = index * slideWidth;
-    track.style.transform = `translateX(-${move}px)`;
-}
- 
-if (next){
-    next.addEventListener("click", () => {
-        index++;
-        if (index >= slides.length) {
-            index = 0;
+    carousels.forEach((carousel) => 
+    {
+        const track = carousel.querySelector(".track");
+        const slides = carousel.querySelectorAll(".slide");
+        const prev = carousel.querySelector(".prev");
+        const next = carousel.querySelector(".next");
+
+        let index = 0; 
+        const slideWidth = 100;
+        
+        function autoSlide()
+        {
+            const move = index * slideWidth;
+            track.style.transform = `translateX(-${move}px)`;
         }
-        autoSlide();
-    })
-}
- 
-if (prev){
-    prev.addEventListener("click", () => {
-        index--;
-        if (index < 0) {
-            index = slides.length - 1;
+
+        if (next)
+        {
+            next.addEventListener("click", () => 
+            {
+                index++;
+                if (index >= slides.length)
+                {
+                    index = 0;
+                }
+                autoSlide();
+            })
         }
-        autoSlide();
-    })
-}
- 
-})
+
+        if (prev) {
+            prev.addEventListener("click", () => 
+            {
+                index--;
+                if (index < 0) 
+                {
+                    index = slides.length - 1;
+                }
+                autoSlide();
+            });
+        }
+    });
+});
